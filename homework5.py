cb70788648a5f8e456c8b4a974cf29d8f73cdc73
exqjk78 <purov.bordan@gmail.com>
2024-06-25 17:56:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var=1,'string',True
+print(immutable_var)
+# Задание 3: Элементы кортежа не поддерживают обращение
+mutable_list=['78','pelmen','Яблоко']
+mutable_list.append(37)
+mutable_list.remove('Яблоко')
+print(mutable_list)
